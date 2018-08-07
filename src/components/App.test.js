a05df458941f57a83fe0d565e876a24274206576
Viiralt <viiralt.h@gmail.com>
2018-08-07 19:19:09 +0300

    finish testing App

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,26 +4,33 @@ import { shallow } from 'enzyme';
 import App from './App';
 import { Button, GiftList } from '../elements';
 
-const wrapper = shallow(<App />);
+describe('App', () => {
+  const wrapper = shallow(<App />);
 
-it('should render the <App /> correctly', () => {
-  expect(wrapper).toMatchSnapshot();
-});
+  it('should render the <App /> correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-it('initialises `state` with an empty list of gifts', () => {
-  // App.state.gifts
-  expect(wrapper.state().gifts).toEqual([]);
-});
+  it('initialises `state` with an empty list of gifts', () => {
+    // App.state.gifts
+    expect(wrapper.state().gifts).toEqual([]);
+  });
 
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-  // assume `add gift` button has been pressed
-  wrapper.find(Button).simulate('click');
+  describe('when clicking the `add gift button`', () => {
+    beforeEach(() => {
+      wrapper.find(Button).simulate('click');
+    });
 
-  expect(wrapper.state().gifts).toEqual([{ id: 1 }]);
-});
+    afterEach(() => {
+      wrapper.setState({ gifts: [] });
+    });
 
-it('adds a new gift to the render list when clicking on the `add gift` button', () => {
-  wrapper.find(Button).simulate('click');
+    it('adds a new gift to `state`', () => {
+      expect(wrapper.state().gifts).toEqual([{ id: 1 }]);
+    });
 
-  expect(wrapper.find(GiftList).children().length).toEqual(2);
+    it('adds a new gift to the render list', () => {
+      expect(wrapper.find(GiftList).children().length).toEqual(1);
+    });
+  });
 });
