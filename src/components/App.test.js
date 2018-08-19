c2c8bdd59d39d048f9d9c6213efbc5435248d0f0
Viiralt <viiralt.h@gmail.com>
2018-08-19 16:43:43 +0200

    App tested

---
+++ src/components/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from './App';
+
+const wrapper = shallow(<App />);
+
+it('renders correctly', () => {
+  expect(wrapper).toMatchSnapshot();
+});
