8809ad9d5ae98697711fb214cc5fe27d9e065c74
Viiralt <viiralt.h@gmail.com>
2018-08-19 16:44:10 +0200

    update snapshots, add setuptests.js

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { Wrapper, Heading } from '../elements';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Heading>Gift Giver</Heading>
+      </Wrapper>
+    );
+  }
+}
