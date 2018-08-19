032e5fb955f054fd318ad78d51f48ac19823932a
Viiralt <viiralt.h@gmail.com>
2018-08-19 17:33:30 +0200

    fix: add new gift test logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 
-import { Wrapper, Heading } from '../elements';
+import { Wrapper, Heading, Button } from '../elements';
 
 export default class App extends Component {
   state = {
     gifts: [],
   };
 
+  handleClick = () => {
+    const { gifts } = this.state;
+    const ids = gifts.map(gift => gift.id);
+    const maxId = ids.length > 0 ? Math.max(...ids) : 0;
+
+    gifts.push({ id: maxId + 1 });
+    this.setState({ gifts });
+  };
+
   render() {
     return (
       <Wrapper>
         <Heading>Gift Giver</Heading>
+        <Button type="submit" onClick={this.handleClick}>
+          Add gift
+        </Button>
       </Wrapper>
     );
   }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from './App';
+import { Button } from '../elements';
 
 const wrapper = shallow(<App />);
 
@@ -11,4 +12,9 @@ it('renders correctly', () => {
 
 it('initialises the `state` with an empty list of gifts', () => {
   expect(wrapper.state('gifts')).toEqual([]);
+});
+
+it('adds new gift to the `state` when clicking the `add gift` button', () => {
+  wrapper.find(Button).simulate('click');
+  expect(wrapper.state('gifts')).toEqual([{ id: 1 }]);
 });
